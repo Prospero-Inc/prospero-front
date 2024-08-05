4e759ec2a31d1ac723d586ba7f3f401e0162efe9
michael <scottrodriguez2k@gmail.com>
2024-08-04 21:58:38 -0600

    fix: cors

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,29 @@
 import { getToken } from 'next-auth/jwt'
 import { NextRequest, NextResponse } from 'next/server'
 
+import { HttpMethod } from './enums'
+
+const allowedOrigins = ['http://31.220.97.169:4000/', 'http://localhost:3000']
+
+// Opciones de CORS
+const corsOptions = {
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+}
+
 export async function middleware(req: NextRequest) {
+  const origin = req.headers.get('origin') ?? ''
+  const isAllowedOrigin = allowedOrigins.includes(origin)
+
+  // Manejo de solicitudes OPTIONS para preflight
+  const isPreflight = req.method === HttpMethod.OPTIONS
+  if (isPreflight) {
+    const preflightHeaders = {
+      ...(isAllowedOrigin && { 'Access-Control-Allow-Origin': origin }),
+      ...corsOptions
+    }
+    return NextResponse.json({}, { headers: preflightHeaders })
+  }
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
   const { pathname } = req.nextUrl
 
